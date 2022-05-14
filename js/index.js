0b48b2dc6176099f8ff2af07c85511f73a0f921e
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2022-05-14 19:15:17 -0300

    added animatedLinks

--- js/index.js
+++ js/index.js
@@ -3,12 +3,22 @@ const links = document.querySelectorAll('.hide')
 const sidebar = document.querySelector('.sidebar')
 
 
+
 menuButton.addEventListener('click', () => {
-    sidebar.classList.toggle('hidesidebar'); hideLinks()
+    sidebar.classList.toggle('hidesidebar');  hideLinks(); animatedLinks()
 })
 
 function hideLinks() {
     links.forEach(link => {
         link.classList.toggle('hide')
     })
-}
+};
+
+function animatedLinks() {
+
+    links.forEach((link) => {
+        link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+    });
+};
